a71a84dff7ab640b7cd95b0db75cbf0127d07389
haiqing.xu <haiqing.xu@ly.com>
2024-12-04 17:52:19 +0800

    init demo

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Add New Item'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
